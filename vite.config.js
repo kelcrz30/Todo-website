23a23e24c8381f75ad46ddf57bbd30f7a4f0b033
Kel <davenykel47@gmail.com>
2025-05-19 17:50:55 +0800

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Todo-website/' // This is important for GitHub Pages
 })
