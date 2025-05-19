5f8d2d5940d8c42cb625f232a483959487fe92f1
Kel <davenykel47@gmail.com>
2025-05-19 17:55:39 +0800

    yes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Todo-website/' // This is important for GitHub Pages
+  base: '/Todo-website/', 
 })
